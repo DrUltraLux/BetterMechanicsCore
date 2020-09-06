3fc456f20ebbf0ebaa6f39b2bf3d3a4dadde7057
Gaiyamato <63906224+Gaiyamato@users.noreply.github.com>
2020-09-06 22:23:55 +1000

    Initial commit

--- src/main/java/com/gaiyamato/bettermechanicscore/BetterMechanicsCore.java
+++ src/main/java/com/gaiyamato/bettermechanicscore/BetterMechanicsCore.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.gaiyamato.bettermechanicscore;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = BetterMechanicsCore.MODID, name = BetterMechanicsCore.NAME, version = BetterMechanicsCore.VERSION)
+public class BetterMechanicsCore
 {
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.0.1";
 
     private static Logger logger;
 
